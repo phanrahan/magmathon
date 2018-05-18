f679eccb9655cd56150fe1fd01ba99bb8c40699d
Pat Hanrahan <hanrahan@cs.stanford.edu>
2018-05-18 10:07:06 -0700

    Added notebooks and tests directories.

--- tests/gold/inout.v
+++
@@ -1,7 +0,0 @@
-module main (input  I1, input  I2, input  I3, input  I4, output  D4, output  D3, output  D2, output  D1);
-assign D4 = I4;
-assign D3 = I3;
-assign D2 = I2;
-assign D1 = I1;
-endmodule
-
--- tests/gold/out.v
+++
@@ -1,5 +0,0 @@
-module main (output  D2, output  D1);
-assign D2 = 1'b1;
-assign D1 = 1'b1;
-endmodule
-

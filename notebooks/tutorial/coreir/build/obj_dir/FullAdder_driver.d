da55019c0af79233ee3ca7aeb3f4fa53cd563bdc
Lenny Truong <lenny@stanford.edu>
2020-02-18 10:57:05 -0800

    Make a pass on fulladder

--- notebooks/tutorial/coreir/build/obj_dir/FullAdder_driver.d
+++
@@ -1,4 +0,0 @@
-FullAdder_driver.o: ../FullAdder_driver.cpp VFullAdder.h \
-  /usr/local/Cellar/verilator/4.026/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/4.026/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/4.026/share/verilator/include/verilated_vcd_c.h
--- notebooks/tutorial/coreir/build/obj_dir/VFullAdder__ALLcls.d
+++
@@ -1,4 +0,0 @@
-VFullAdder__ALLcls.o: VFullAdder__ALLcls.cpp VFullAdder.cpp VFullAdder.h \
-  /usr/local/Cellar/verilator/4.026/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/4.026/share/verilator/include/verilatedos.h \
-  VFullAdder__Syms.h
